94672121ee15998dd3978ab7d6d0b81d71c6a729
Ivan <gonzalesmasrcasivan@gmail.com>
2021-11-05 21:54:13 -0500

    05/11/2021 cambios eliminar y nuevo registro

--- models/Producto.php
+++ models/Producto.php
@@ -27,29 +27,28 @@ class Producto extends Conectar{
     }
 
     public function delete_producto($prod_id){
-        $conectar = parent::conexion();
+        $conectar= parent::conexion();
         parent::set_names();
         $sql="UPDATE tm_producto
-             SET
+            SET
                 est=0,
-                fech_elim = now()
-                WHERE
+                fech_elim=now()
+            WHERE
                 prod_id = ?";
-        $sql=$conectar->prepare($prod_id);
+        $sql=$conectar->prepare($sql);
         $sql->bindValue(1,$prod_id);
         $sql->execute();
         return $resultado=$sql->fetchAll();
-
     }
 
     public function insert_producto($prod_nom){
-     $conectar = parent::conexion();
-     parent::set_names();
-     $sql="INSERT INTO tm_producto (prod_id, prod_nom, ech_crea, fech_mod, fech_elim, est) VALUE (NULL,?,now(),NULL,NULL,1);";
-     $sql=$conectar->prepare($sql);
-     $sql->bindValue(1,$prod_id);
-     $sql->execute();
-     return $resultado=$sql->fetchAll();
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="INSERT INTO tm_producto (prod_id, prod_nom, fech_crea, fech_mod, fech_elim, est) VALUES (NULL, ?, now(), NULL, NULL, 1);";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1,$prod_nom);
+        $sql->execute();
+        return $resultado=$sql->fetchAll();
     }
 
     public function update_producto($prod_id,$prod_nom){
